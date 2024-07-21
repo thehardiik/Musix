f0269d4a0b69db0890b403f1b7f5a9c5e21b0d92
Hardik Srivastav <hardiknjms21@gmail.com>
2024-07-21 15:51:59 +0530

    Testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCard from "@/components/TestimonialCard";
+import WhyChooseUs from "@/components/ui/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection/>
         <FeaturedSection/>
+        <WhyChooseUs/>
+        <TestimonialCard/>
     </main>
   );
 }
